839bf9cda4486d92baf663f4b0fca4177c7e1439
Mobin Dariush <mobindariush@outlook.com>
2022-06-04 21:12:35 +0430

    Add policy ranking test

---
+++ src/test/kotlin/stochastic/multiqueue/TestDoubleQueueDelayRanking.kt
@@ -0,0 +1,58 @@
+package stochastic.multiqueue
+
+import com.google.common.truth.Truth.assertThat
+import core.policy.AlphaRange
+import core.ue.OffloadingSystemConfig.Companion.withNumberOfSections
+import core.ue.OffloadingSystemConfig.Companion.withTaskQueueCapacity
+import org.junit.jupiter.api.Test
+import simulation.app.Mock
+import tester.PolicyRankingTester
+import kotlin.system.measureTimeMillis
+
+class TestDoubleQueueDelayRanking {
+
+    @Test
+    fun testCompareConcurrentWithSingleThread() {
+        val tester = PolicyRankingTester(
+            baseSystemConfig = Mock.doubleConfigHeavyLight().withNumberOfSections(listOf(3, 2)).withTaskQueueCapacity(5),
+            alphaRanges = listOf(AlphaRange.Constant(0.2), AlphaRange.Variable(0.01, 0.3, 10)),
+            precision = 4,
+            simulationTicks = 1_000_000,
+            assertionsEnabled = true
+        )
+
+        val resultsSingleThread: PolicyRankingTester.DelayAverageRankingResult
+        val millisSingle = measureTimeMillis {
+            resultsSingleThread = tester.run()
+        }
+
+        val resultsConcurrent: PolicyRankingTester.DelayAverageRankingResult
+        val millisConcurrent = measureTimeMillis {
+            resultsConcurrent = tester.runConcurrent(4)
+        }
+
+        resultsSingleThread.stochasticRankingPercents.forEachIndexed { index, d ->
+            assertThat(d).isWithin(1e-3).of(resultsConcurrent.stochasticRankingPercents[index])
+        }
+
+        resultsSingleThread.localOnlyRankingPercents.forEachIndexed { index, d ->
+            assertThat(d).isWithin(1e-3).of(resultsConcurrent.localOnlyRankingPercents[index])
+        }
+
+        resultsSingleThread.offloadOnlyRankingPercents.forEachIndexed { index, d ->
+            assertThat(d).isWithin(1e-3).of(resultsConcurrent.offloadOnlyRankingPercents[index])
+        }
+
+        resultsSingleThread.greedyLocalFirstRankingPercents.forEachIndexed { index, d ->
+            assertThat(d).isWithin(1e-3).of(resultsConcurrent.greedyLocalFirstRankingPercents[index])
+        }
+
+        resultsSingleThread.greedyOffloadFirstRankingPercents.forEachIndexed { index, d ->
+            assertThat(d).isWithin(1e-3).of(resultsConcurrent.greedyOffloadFirstRankingPercents[index])
+        }
+
+        println("Single thread running time = $millisSingle | Multi thread running time = $millisConcurrent")
+
+        // Run in 5d8cec1: Single thread running time = 218994 | Multi thread running time = 100750
+    }
+}
